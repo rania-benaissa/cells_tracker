274900ce09aab7291219d80c3180f65e3934e22c
Rania Benaissa <ranbenaissa@gmail.com>
2022-11-19 15:32:18 +0100

    op

--- how_to_load_my_data.py
+++ how_to_load_my_data.py
@@ -11,3 +11,17 @@
 
 # print("Nb images ", countImages(images, "sequences"))
 # animateSequence(images[0])
+
+
+#from deepcell.datasets.tracked import hela_s3 as hela
+# from utils import *
+
+# filename = 'hela_tracking.trks'
+# (X_train, y_train), (X_test, y_test) = hela.load_tracked_data(filename)
+
+
+# print("train images", X_train.shape)
+# print("train labels", y_train.shape)
+# print("test images", X_test.shape)
+# print("test labels", y_test.shape)
+
--- program.py
+++ program.py
@@ -1,11 +1,6 @@
-from deepcell.datasets.tracked import hela_s3 as hela
 from utils import *
 
-filename = 'hela_tracking.trks'
-(X_train, y_train), (X_test, y_test) = hela.load_tracked_data(filename)
+images = load_folder("../Images/01")
 
-
-print("train images", X_train.shape)
-print("train labels", y_train.shape)
-print("test images", X_test.shape)
-print("test labels", y_test.shape)
+print("Nb images ", countImages(images))
+animateSequence(images)
